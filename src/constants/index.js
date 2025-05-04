b81b47d2b0f96a15cd3bd3cd2db1af3075b5d666
Kenneth <kenneth@stage12.nl>
2025-05-04 14:57:28 +0200

    V2

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const defaultIpfsGateway = 'https://api.universalprofile.cloud/ipfs/'
-export const followMeAddress = '0xD65A31d205bCdfe29dAA29F3aB0f24a00Ba4be58'
+export const followMeAddress = '0x1a4a489A1C18838ee3Fb9788B82620b907CE4A96'
 export const LSP26FollowerSystemAddress = '0xf01103E5a9909Fc0DBe8166dA7085e0285daDDcA'
